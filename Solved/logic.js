5e1ad45937865c7f0cf9ce82307dd498a3b0bba2
SPonevil <50465125+SPonevil@users.noreply.github.com>
2019-09-18 21:12:39 -0700

    New map final

--- Solved/logic.js
+++ Solved/logic.js
@@ -1,11 +1,23 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = "sf1.geoJSON";
-
+var sfdisurl = "sfdis.geoJSON";
+var sfpop = "hood.csv"
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
+  var earth = createFeatures(data.features);
+
+  d3.csv(sfpop, function(csv) {
+
+  d3.json(sfdisurl, function(data) {
+// console.log(csv)
+  var sfwhatever = sfDis(data.features, csv);
+
+ createMap(earth, sfwhatever)
+
+        });
+    });
 });
 
 function createFeatures(earthquakeData) {
@@ -24,13 +36,37 @@ function createFeatures(earthquakeData) {
   });
 
   // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
+  return earthquakes
 }
 
-function createMap(earthquakes) {
+function sfDis(sfData, csv) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function sfDis1(feature, layer) {
+    console.log(csv)
+    // csv.filter
+    var csvData = csv.filter(data => data["Indicator Categoy"].trim() == feature.properties.nhood.trim())
+    console.log(csvData)
+    layer.bindPopup("<h3>" + feature.properties.nhood +
+      // "</h3><hr><p>" + new Date(feature.properties.nhood) + "</p>");
+      "</h3><hr><p>" + csvData[0].Demographics + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var sf = L.geoJSON(sfData, {
+    onEachFeature: sfDis1
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  return sf
+}
+
+function createMap(earthquakes, sf) {
 
   // Define streetmap and darkmap layers
-  console.log(earthquakes)
+  // console.log(earthquakes)
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -53,14 +89,16 @@ function createMap(earthquakes) {
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
-    Earthquakes: earthquakes
+    Earthquakes: earthquakes, sf: sf
   };
+
+
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [37.7749, -122.4194],
     zoom: 13,
-    layers: [streetmap, earthquakes]
+    layers: [streetmap, earthquakes, sf]
   });
 
   // Create a layer control
